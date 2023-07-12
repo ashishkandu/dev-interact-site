5a750b66765c3ad90fd5ea1b24872c48c086a4ec
ashishkandu <ashishkandu43@gmail.com>
2023-07-12 22:38:25 +0000

    Integration of sqlite database

--- base/views.py
+++ base/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Room
 
 
-rooms = [
-    {'id': 1, 'name': 'Lets learn Django'},
-    {'id': 2, 'name': 'Design Class'},
-    {'id': 3, 'name': 'Frontenders !'},
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn Django'},
+#     {'id': 2, 'name': 'Design Class'},
+#     {'id': 3, 'name': 'Frontenders !'},
 
-]
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == pk:
-            room = i
-            break
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
