e5c3769835bb3a65b041773e9481893722682c54
ashishkandu <ashishkandu43@gmail.com>
2023-07-13 18:59:06 +0000

    crud implementaion

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('room/<int:pk>/', views.room, name='room')
+    path('room/<int:pk>/', views.room, name='room'),
+
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<int:pk>/', views.updateRoom, name='update-room'),
+    path('delete-room/<int:pk>/', views.deleteRoom, name='delete-room'),
+
+
 ]
