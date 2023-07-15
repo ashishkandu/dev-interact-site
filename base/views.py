28575fb5fb779caba4868d521d14563b55934763
ashishkandu <ashishkandu43@gmail.com>
2023-07-15 14:18:05 +0000

    messages upport

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # rooms = [
@@ -76,14 +76,28 @@ def home(request):
     )
     topics = Topic.objects.all()
     rooms_count = rooms.count()
+    all_messages = Message.objects.all()
 
-    context = {'rooms': rooms, 'topics': topics, 'rooms_count': rooms_count}
+    context = {'rooms': rooms, 'topics': topics,
+               'rooms_count': rooms_count, 'all_messages': all_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    context = {'room': room, 'room_messages': room_messages,
+               'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -128,3 +142,16 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Not allowed!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
