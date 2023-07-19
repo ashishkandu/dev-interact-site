a684c035025f3f534af1a7a5c1c30e6e653d8d24
ashishkandu <ashishkandu43@gmail.com>
2023-07-19 19:37:45 +0545

    add missing styles

--- base/urls.py
+++ base/urls.py
@@ -13,8 +13,9 @@ urlpatterns = [
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<int:pk>/', views.updateRoom, name='update-room'),
     path('delete-room/<int:pk>/', views.deleteRoom, name='delete-room'),
-    path('delete-message/<int:pk>/', views.deleteMessage, name='delete-message')
+    path('delete-message/<int:pk>/', views.deleteMessage, name='delete-message'),
 
+    path('update-user/', views.updateUser, name='update-user'),
 
 
 ]
